02384ca8f3266730fdf6e082e61c63361931232f
Marty Warfel <marthawarfel@gmail.com>
2020-11-28 19:42:31 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -24,11 +24,11 @@ def literal_aoh
    { name:  "Kate Gompert", occupation: "None" },
    { name:  "Bruce Green", occupation: "Fan of Mildred" }
  ]
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
+  
 end
 
 def aoh_lookup(aoh, row, key)
+  aoh[2][:name]
 end
 
 def aoh_update(aoh, row, key, new_value)
