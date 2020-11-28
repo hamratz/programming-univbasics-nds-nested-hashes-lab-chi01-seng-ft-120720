e467793b33b2e80f5a650441909a70b5a1d8e6eb
Marty Warfel <marthawarfel@gmail.com>
2020-11-28 19:39:58 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,12 +13,14 @@ def assembled_aoh
    PAT_M,
    KATE_G,
    BRUCE_G]
- 
- # Build an array that contains (or, "nests") the constants into a single
-  # Array. Ruby constants are denoted by ALL_CAPS
+
 end
 
 def literal_aoh
+ array_literal -[
+   {name:  "Don Gately", occupation: "Live-in Staff" },
+   
+ ]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
