fff3be90bee740ef11d5e9a330d026b4f3994292
Marty Warfel <marthawarfel@gmail.com>
2020-11-28 19:49:39 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -33,7 +33,7 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   
-  AoH[1][:name] ="new_value"
+  AoH[1][:name] ="Joelle van Dyne (PGOAT)"
   AoH[1][:name]
   
   # Update the AoH data at row and key to have the value of new_value
